da587b1b2d1dff3bbc1e8d18b6b15b21585038c4
WangYiran01 <129300550+WangYiran01@users.noreply.github.com>
2024-02-22 17:52:33 +0800

    Update TEST1.lean

--- TEST1.lean
+++ TEST1.lean
@@ -6,4 +6,5 @@ open Finset
 
 open BigOperators
 
-theorem choose_mul_eq_choose_mul(hkn : k ≤ n) (hsk : 1 ≤ k) : choose n k * choose k 1 = choose n 1 * choose (n - 1) (k - 1)  := by rw[choose_mul hkn hsk]
+theorem choose_mul_eq_choose_mul(hkn : k ≤ n) (hsk : 1 ≤ k) : choose n k * choose k 1 = choose n 1 * choose (n - 1) (k - 1)  := by 
+  rw[choose_mul hkn hsk]
